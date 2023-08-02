4f8652b6e2a9c7ed124b48b984748ac0f15fae1c
kawajiharuka <kjn.hk10001@gmail.com>
2023-08-03 08:06:07 +0900

    Logo、Nav新規作成

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Logo from "./Logo";
+import Nav from "./Nav";
 
 const Header = () => {
-  return <header>HEADER</header>;
+  return (
+    <header>
+      <Logo />
+      <Nav />
+    </header>
+  );
 };
 
 export default Header;
---
+++ app/components/Logo.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import React from "react";
+
+const Logo = () => {
+  return <Link href={"/"}>CUBE</Link>;
+};
+
+export default Logo;
