2ffc7a52494a101e73ccff88ff5df04b7355c8de
kawajiharuka <kjn.hk10001@gmail.com>
2023-08-08 08:20:00 +0900

    ２段組みのコンポーネント作成

---
+++ app/components/TwoColumn.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from "react";
+import styles from "../styles/two-column.module.css";
+
+export function TwoColumn({ children }: { children: ReactNode }) {
+  return <div className={styles.flexContainer}>{children}</div>;
+}
+
+export function TwoColumnMain({ children }: { children: ReactNode }) {
+  return <div className={styles.main}>{children}</div>;
+}
+
+export function TwoColumnSidebar({ children }: { children: ReactNode }) {
+  return <div className={styles.sidebar}>{children}</div>;
+}
