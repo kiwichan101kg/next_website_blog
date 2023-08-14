130ba33fa68f094e89c31a58499ac953a6e6a4f8
kawajiharuka <kjn.hk10001@gmail.com>
2023-08-14 16:19:13 +0900

    Hero　画像設定

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "../styles/hero.module.css";
+import Image from "next/image";
+import cube from "../images/cube.jpg";
 
 type HeroProps = {
   title: string;
@@ -15,7 +17,20 @@ const Hero = ({ title, subTitle, imageOn = false }: HeroProps) => {
           <h1 className={styles.title}>{title}</h1>
           <p className={styles.subtitle}>{subTitle}</p>
         </div>
-        {imageOn && <figure>[画像]</figure>}
+        {imageOn && (
+          <figure className={styles.image}>
+            <Image
+              src={cube}
+              alt="空飛ぶロケット"
+              width={1980}
+              height={1150}
+              // sizes="(min-width:1152px)576px,(min-width:768px)50vw,100vw"
+              layout="responsive"
+              priority
+              placeholder="blur"
+            />
+          </figure>
+        )}
       </div>
     </div>
   );
