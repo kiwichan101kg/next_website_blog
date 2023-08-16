7826d5b8214d2de2b793a81e8619549e54a08b51
kawajiharuka <kjn.hk10001@gmail.com>
2023-08-16 23:03:55 +0900

    ページネーション作成

---
+++ app/blog/[slug]/Pagenation.tsx
@@ -0,0 +1,40 @@
+import {
+  faSquareCaretLeft,
+  faSquareCaretRight,
+} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import React from "react";
+import styles from "../styles/pagenation.module.css";
+
+type prevNextPost = {
+  prevText: string;
+  prevUrl: string;
+  nextText: string;
+  nextUrl: string;
+};
+
+const Pagenation = ({ prevText, prevUrl, nextText, nextUrl }: prevNextPost) => {
+  return (
+    <ul className={styles.flexContainer}>
+      {prevText && prevUrl && (
+        <li>
+          <Link href={prevUrl} className={styles.iconText}>
+            <FontAwesomeIcon icon={faSquareCaretLeft} color="var(--gray-25)" />
+            <span>{prevText}</span>
+          </Link>
+        </li>
+      )}
+      {nextText && nextUrl && (
+        <li className={styles.next}>
+          <Link href={nextUrl} className={styles.iconText}>
+            <span>{nextText}</span>
+            <FontAwesomeIcon icon={faSquareCaretRight} color="var(--gray-25)" />
+          </Link>
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default Pagenation;
--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import Container from "@/app/components/Container";
-import { getPostBySlug } from "@/app/lib/api";
+import { getAllSlugs, getPostBySlug } from "@/app/lib/api";
 import React from "react";
 import PostHeader, { PostHeaderProps } from "./PostHeader";
 import Image from "next/image";
@@ -9,18 +9,26 @@ import {
   TwoColumnMain,
   TwoColumnSidebar,
 } from "@/app/components/TwoColumn";
-import ConvertBody from "./COnvertBody";
+import ConvertBody from "./ConvertBody";
 import PostCategories from "./PostCategories";
 import { eyecatchLocal } from "@/app/lib/constants";
+import Pagenation from "./Pagenation";
 
 // urlのidはpropsのparamsに含まれる
 type Props = { params: { slug: string } };
+
+type SlugType = {
+  title: string;
+  slug: string;
+};
 
 const page = async ({ params: { slug } }: Props) => {
   const post = await getPostBySlug(slug);
   const { title, publishDate, eyecatch: _eyecatch, content, categories } = post;
   const eyecatch = _eyecatch ?? eyecatchLocal;
-  console.log("ブログ", post);
+
+  const allSlugs = await getAllSlugs();
+  const [prevPost, nextPost] = prevNextPost(allSlugs, slug);
 
   const postHeaderProps: PostHeaderProps = {
     title,
@@ -52,9 +60,35 @@ const page = async ({ params: { slug } }: Props) => {
             <PostCategories categories={categories} />
           </TwoColumnSidebar>
         </TwoColumn>
+        <Pagenation
+          prevText={prevPost.title}
+          prevUrl={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
       </article>
     </Container>
   );
+};
+
+export const prevNextPost = (
+  allSlugs: SlugType[],
+  currentSlug: SlugType["slug"]
+) => {
+  const numberOfPosts = allSlugs.length;
+  // console.log("slugの数", numberOfPosts);
+  console.log(allSlugs);
+
+  // 取得したslug配列の中から、現在のslugと一致するものの配列のindexを返却する
+  const index = allSlugs.findIndex(({ slug }) => slug === currentSlug);
+  console.log("index", index);
+
+  const prevPost =
+    index + 1 === numberOfPosts ? { title: "", slug: "" } : allSlugs[index + 1];
+
+  const nextPost = index === 0 ? { title: "", slug: "" } : allSlugs[index - 1];
+
+  return [prevPost, nextPost];
 };
 
 export default page;
