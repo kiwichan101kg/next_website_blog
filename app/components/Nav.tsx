60ee151e44047d9201fa7392da8ef2e1400ea12d
kawajiharuka <kjn.hk10001@gmail.com>
2023-08-25 23:11:54 +0900

    ハンバーガーメニュー作成

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,19 +1,44 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/nav.module.css";
 
 const Nav = () => {
+  const [navIsOpen, setNavIsOpen] = useState<boolean>(false);
   return (
-    <nav>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      {navIsOpen && (
+        <style jsx global>{`
+          @media (max-width: 767px) {
+            body {
+              overflow: hidden;
+              position: fixed;
+              width: 100%;
+            }
+          }
+        `}</style>
+      )}
+      <button
+        className={styles.btn}
+        onClick={() => setNavIsOpen((prev) => !prev)}
+      >
+        <span className={styles.bar}></span>
+      </button>
       <ul className={styles.list}>
         <li>
-          <Link href={"/"}>Home</Link>
+          <Link href={"/"} onClick={() => setNavIsOpen(false)}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link href={"/about"}>About</Link>
+          <Link href={"/about"} onClick={() => setNavIsOpen(false)}>
+            About
+          </Link>
         </li>
         <li>
-          <Link href={"/blog"}>Blog</Link>
+          <Link href={"/blog"} onClick={() => setNavIsOpen(false)}>
+            Blog
+          </Link>
         </li>
       </ul>
     </nav>
